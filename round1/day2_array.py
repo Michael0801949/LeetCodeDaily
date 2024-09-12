6d3d7fa694b98087a94c45d339796b3b909b9f0f
Michael0801949 <48659868+Michael0801949@users.noreply.github.com>
2024-09-12 15:39:45 -0700

    Update day2_array.py
    day2 q 2

--- round1/day2_array.py
+++ round1/day2_array.py
@@ -34,7 +34,7 @@ class Solution:
             sum_array += nums[j]
             while sum_array >= target:
                 sub_length = j - i + 1
-                if result is None:
+                if result == float('inf'):
                      result = sub_length
                 else:
                     result = min(result, sub_length)
@@ -44,3 +44,38 @@ class Solution:
             return result
         else: 
             return 0 
+
+# Q2 sprial matrix https://leetcode.com/problems/spiral-matrix-ii/description/
+
+'''
+Need to think through 
+1. how to iterate through each side of the matrix
+2. how to draw a line between each iteration
+This problem have a high requirement on implimention a logic, the logic itself is not hard to get.
+'''
+#Solution
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        nums = [[0] * n for _ in range(n)] # remember how to generate a n*n matrix
+        startx, starty = 0, 0               # start point
+        loop, mid = n // 2, n // 2          # number of iteration、if n is odd the center of matrix
+        count = 1                           # counter of each number 1,2,3,4...until n
+        for offset in range(1, loop + 1) :      # offset + 1 every iteration，starting from 1, range helps here no need to define offset, offset record how many circle we have looped it should be n/2, range() is left close right open so need +1
+            for i in range(starty, n - offset) :    # from left to right, left close right open
+                nums[startx][i] = count
+                count += 1
+            for i in range(startx, n - offset) :    # top to bottom
+                nums[i][n - offset] = count
+                count += 1
+            for i in range(n - offset, starty, -1) : # right to left
+                nums[n - offset][i] = count
+                count += 1
+            for i in range(n - offset, startx, -1) : # bottom to top
+                nums[i][starty] = count
+                count += 1                
+            startx += 1         # renew start point
+            starty += 1
+
+        if n % 2 != 0 :			# if n is odd, fill the center of matrix
+            nums[mid][mid] = count 
+        return nums
