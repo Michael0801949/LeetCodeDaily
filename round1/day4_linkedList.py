5b60e572b70cc79c949c22fd73b6540c0531439a
Michael0801949 <48659868+Michael0801949@users.noreply.github.com>
2024-09-15 14:53:41 -0700

    Update day4_linkedList.py
    day4_linkedList_Q1, more detail

--- round1/day4_linkedList.py
+++ round1/day4_linkedList.py
@@ -22,12 +22,12 @@ class Solution:
         dummy_head = ListNode(None,head)
         curr = dummy_head # curr is the pointer to the node before 2 swap nodes, use it allow use to have access to curr.next and curr.next.next as while condition
         while curr.next and curr.next.next:
-            temp1 = curr.next
-            temp2 = curr.next.next.next
-
-            curr.next = curr.next.next # this is the most repeatable swap order, more detail see: https://programmercarl.com/0024.%E4%B8%A4%E4%B8%A4%E4%BA%A4%E6%8D%A2%E9%93%BE%E8%A1%A8%E4%B8%AD%E7%9A%84%E8%8A%82%E7%82%B9.html#%E6%80%9D%E8%B7%AF
-            curr.next.next = temp1
-            temp1.next = temp2
-            curr = curr.next.next
+            temp1 = curr.next # isolate node1
+            temp2 = curr.next.next.next # isolate node2
+            # this is the most repeatable swap order, more detail see: https://programmercarl.com/0024.%E4%B8%A4%E4%B8%A4%E4%BA%A4%E6%8D%A2%E9%93%BE%E8%A1%A8%E4%B8%AD%E7%9A%84%E8%8A%82%E7%82%B9.html#%E6%80%9D%E8%B7%AF
+            curr.next = curr.next.next # current node next next node become the next node
+            curr.next.next = temp1 # revert the pointer between the old next node(temp1) and new next node
+            temp1.next = temp2 # point the new next next node next pointer to the temp2
+            curr = curr.next.next # move forward to the next swap if there are still 2 or 2+ node left
         return dummy_head.next
       
