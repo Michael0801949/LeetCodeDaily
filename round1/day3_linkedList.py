23c525ffe4daf1d44db21ce42b61b38c89972632
Michael0801949 <48659868+Michael0801949@users.noreply.github.com>
2024-09-14 19:33:57 -0700

    Update day3_linkedList.py
    R1_Day3_Q3-1

--- round1/day3_linkedList.py
+++ round1/day3_linkedList.py
@@ -102,3 +102,25 @@ class MyLinkedList:
             curr = curr.next
         curr.next = curr.next.next
         self.size -= 1
+        
+# Q3 Reverse Linked List https://leetcode.com/problems/reverse-linked-list/description/
+
+# 2 pointer
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        pre = None # pre is just a pointer point to the previous node
+        while curr:
+            temp = curr.next
+            curr.next = pre
+            pre = curr
+            curr = temp
+        return pre
+
+
