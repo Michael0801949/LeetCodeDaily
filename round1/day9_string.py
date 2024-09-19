e1054923f78d32ef93451ade077a51fbd455438a
Michael0801949 <48659868+Michael0801949@users.noreply.github.com>
2024-09-19 13:04:03 -0700

    Update day9_string.py Q3
    R1_day9string_Q3Find the Index of the First Occurrence in a String

--- round1/day9_string.py
+++ round1/day9_string.py
@@ -23,3 +23,28 @@ class Solution:
         s = s[::-1]
         s = ' '.join(word[::-1] for word in s.split())
         return s
+
+#Q2 right rotate string: https://kamacoder.com/problempage.php?pid=1065
+
+'''
+for python this is a list problem, need extra space
+'''
+k = int(input())
+s = input()
+
+s = s[len(s)-k:] + s[:len(s)-k]
+print(s)
+
+#Q3 Find the Index of the First Occurrence in a String
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        hlen = len(haystack)
+        nlen = len(needle)
+        i = 0
+        while i <= hlen - nlen: # blundary here need to be <=, need to continue even if rest h = n, extre case h == n == 'a'
+            if haystack[i] == needle[0]:
+                if haystack[i:i+nlen] == needle:
+                    return i
+            i += 1
+        return -1
